09bdc3cf92789a923e1f2701481dd24ff346beda
Victor <victorblancogarcia3@gmail.com>
2020-03-19 22:50:36 +0100

    Jenkinsfile

--- functions/jest.config.js
+++ functions/jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testPathIgnorePatterns: ['lib/', 'node_modules/'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     rootDir: 'src'
 }
 
