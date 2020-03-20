22aee5e14d931a8b9c045ab2fc2c3b0d758fe98f
Joan <Joan@DESKTOP-DQID8I5>
2020-03-20 19:34:23 +0100

    Add all-pocks.ts file and the endpoint

--- functions/src/controllers/pocks-controller.ts
+++ functions/src/controllers/pocks-controller.ts
@@ -1,11 +1,12 @@
 import { CreatePockRestInput } from "@/modules/pocks/models/create-pock-rest-input"
 import { PockMessage } from "@/modules/pocks/models/pock-message"
-import { Path, POST, PreProcessor } from "typescript-rest"
+import { GET, Path, POST, PreProcessor } from "typescript-rest"
 import { Tags } from "typescript-rest-swagger";
 import { AppClient } from "../common/auth/app-client"
 import { appClientAuthenticator } from "../common/auth/app-client-authenticator"
 import createPock from "../modules/pocks/actions/create-pock"
 import { BaseController } from "./base-controller"
+import allPocks from "../modules/pocks/actions/all-pocks"
 
 /**
  * Pocks rest controller that manages all the endpoints that are in /pock.
@@ -18,4 +19,19 @@ export class PocksRestController extends BaseController {
     async createPockHandler(body: CreatePockRestInput): Promise<PockMessage> {
         return this.asPromise(createPock(body))
     }
+}
+
+/**
+ * Message rest controller it manages all the endpoint that are in /messages
+ */
+@Tags('All Pocks')
+@Path('/pocks')
+export class MessageRestController extends BaseController {
+    @PreProcessor(appClientAuthenticator([ AppClient.POCKLES ]))
+    @GET
+    async getAllMessagesHandler(): Promise<PockMessage[]> {
+        return this.asPromise(allPocks())
+    }
+
+
 }
---
+++ functions/src/modules/pocks/actions/all-pocks.ts
@@ -0,0 +1,52 @@
+import * as admin from "firebase-admin";
+import { ErrorResponse } from "../../../common/error";
+import addLocation from '../../geolocation/actions/add-location'
+import { CreatePockRestInput, validateCreatePockRestInput } from "../models/create-pock-rest-input"
+import { PockMessage } from "../models/pock-message"
+
+export const MESSAGES_REF = '/messages'
+export const DEFAULT_EXPIRATION_TIME = 7 * 24 * 3600 * 1000 // 1 week
+
+/**
+ * Inserts a new pock on the database.
+ *
+ * Note: It could be made with a transaction instead of the 'rollback' it
+ * is applying in the try-catch.
+ *
+ * @param input
+ */
+export default async (): Promise<PockMessage[]> => {
+    // Step 1: validate input. ¡Nothing must be validated!
+    let returnPocksList : PockMessage[] = []
+
+    // Step 2: Get all Pocks from DataBase
+    const snapshot = await admin.database().ref(`${MESSAGES_REF}`).once('value')
+    if (!snapshot) {
+        throw new ErrorResponse(418, 'Could not get all pocks')
+    }
+
+    for (let pock of snapshot.val()){
+        const {
+            key,
+            message,
+            location,
+            dateInserted,
+            mediaUrl,
+            category,
+            chatAccess
+        } = pock.val()
+
+        returnPocksList.push({
+            id:key,
+            message,
+            location,
+            dateInserted,
+            category,
+            chatAccess: !!chatAccess ? chatAccess : false,
+            media: mediaUrl,
+            user: '0'
+            })
+    }
+
+    return returnPocksList
+}
