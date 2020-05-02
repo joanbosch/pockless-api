7ef73f62b5a49e4d6efaea338d568de95c35ad60
Sergio Vázquez <sergiovazquezmontesdeoca@gmail.com>
2020-05-02 13:05:03 +0200

    Cleaning

--- functions/src/modules/messaging/actions/insert-token.ts
+++ functions/src/modules/messaging/actions/insert-token.ts
@@ -1,9 +1,10 @@
 import {InsertTokenRestInput} from "../../user/model/insert-token-rest-input";
 import * as admin from "firebase-admin";
+import {PROFILE_REF} from "../../../common/paths";
 
 export default async (body: InsertTokenRestInput, user: any) => {
 
-    await admin.database().ref(`profile/${user.uid}/token`).set(
+    await admin.database().ref(`${PROFILE_REF}/${user.uid}/token`).set(
         body.token
     )
 
