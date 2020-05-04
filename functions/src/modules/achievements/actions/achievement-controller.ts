8bcfcca8c0f6efc4da408f497d1f6bbb4508bfbc
Vmorais22 <victormoraisllahi@gmail.com>
2020-05-04 19:48:00 +0200

    -Added achievements in the database -Implemented get all Ids of an user

--- functions/src/modules/achievements/actions/achievement-controller.ts
+++ functions/src/modules/achievements/actions/achievement-controller.ts
@@ -1,4 +1,29 @@
 import * as admin from "firebase-admin";
 import { ErrorResponse } from "../../../common/error";
-import { MESSAGES_REF } from "../../../common/paths";
+import {MESSAGES_REF, PROFILE_REF} from "../../../common/paths";
+import { OwnedAchievements } from "../models/ownedAchievements";
+import {LatLong} from "../../../common/models/lat-long";
+import {now} from "moment";
+import {Achievements} from "../models/achievements";
 
+/**
+ * Return the user associated with the token
+ * @param user
+ */
+
+export default async (user: any): Promise<string[]> => {
+
+    //Step 1: Take an Snapshot of the achievments
+    const snapshotOfAllAchivments = await admin.database().ref('/achievements')
+        .once('value')
+
+    //Step2: Get All achievementsId where userId is equal to the user given
+    const allAchievmentsOfAnUser: string[] = []
+    snapshotOfAllAchivments.forEach((s: admin.database.DataSnapshot) => {
+        if (s.val().userId == user)
+            allAchievmentsOfAnUser.push(s.val().achievementId)
+    })
+
+    //Step 3: Return all achievementsId
+    return allAchievmentsOfAnUser
+}
--- functions/src/modules/achievements/models/achievements.ts
+++ functions/src/modules/achievements/models/achievements.ts
@@ -1,10 +1,12 @@
-class Achievements {
-    achievementId: number;
+import * as admin from "firebase-admin";
+
+export class Achievements {
+    achievementId: string;
     achievementName: string;
     description: string;
     achievementIcon: string;
 
-    constructor(theId: number, theName: string, theDescription: string, theIcon: string) {
+    constructor(theId: string, theName: string, theDescription: string, theIcon: string) {
         this.achievementId = theId;
         this.achievementName = theName;
         this.description = theDescription;
@@ -12,27 +14,27 @@ class Achievements {
     }
 }
 
-new Achievements ( 0, "¡Bienvenido a Pockles!", "Te has unido a la familia Pockles", "hola")
-new Achievements ( 1, "¡Primer Pock!", "Has publicado tu primer pock. ¡Enhorabuena!", "hola")
-new Achievements ( 2, "¡10 Pocks!", "Has publicado tu décimo pock. ¡Enhorabuena!", "hola")
-new Achievements ( 3, "¡100 Pocks!", "Has publicado tu centésimo pock. ¡Enhorabuena!", "hola")
-new Achievements ( 4, "¡1000 Pocks!", "Has publicado tu milésimo pock. ¡Enhorabuena!", "hola")
-new Achievements ( 5, "¡Tienes 10 Me Gusta!", "Uno de tus Pocks ha gustado a 10 personas.", "hola")
-new Achievements ( 6, "¡Tienes 100 Me Gusta!", "Uno de tus Pocks ha gustado a 100 personas ¡WOW!", "hola")
-new Achievements ( 7, "¡Has chateado con 5 personas!", "Has iniciado una conversación con 5 usuarios.", "hola")
-new Achievements ( 8, "¡Has filtrado la lista de Pocks!", "No todo quieren ver las mismas cosas...", "hola")
-new Achievements ( 9, "¡Has sido reportado!", "Cuidado que sé donde vives, ¿eh? Vamos a portarnos bien.", "hola")
-new Achievements ( 10, "¡Has usado el mapa de calor!", "Que calor hace... ¡Ah no! Que son los Pocks.", "hola")
-new Achievements ( 11, "¡Menuda puntería!", "Has colocado dos Pocks exactamente en las mismas coordenadas.", "hola")
-new Achievements ( 12, "¡Has mirado tu historial de Pocks!", "Has mirado tu registro de Pocks. ¿De verdad pusiste eso?", "hola")
-new Achievements ( 13, "Llevas un año en Pockles", "Nadie se lo esperaba. Cuuumpleaños Feeeliz...", "hola")
-new Achievements ( 14, "¡Conseguiste todos los logros de Pockles!", "¡Eres un maestro de Pockles!", "hola")
-new Achievements ( 15, "Secreto de Pockles nº1", "EASTER EGG: Por crear 517 Pocks", "hola")
-new Achievements ( 16, "Secreto de Pockles nº2", "EASTER EGG: Por crear 13 Pocks de la categoría SALUD", "hola")
-new Achievements ( 17, "Secreto de Pockles nº3", "EASTER EGG: Por no poner ningún Pock durante un mes", "hola")
-new Achievements ( 18, "Secreto de Pockles nº4", "EASTER EGG: Por poner un Pock a las 8:00 AM", "hola")
-new Achievements ( 19, "Secreto de Pockles nº5", "EASTER EGG: Por mandarte 100 mensajes en un chat", "hola")
-new Achievements ( 20, "Secreto de Pockles nº6", "EASTER EGG: Por conseguir todos los logros normales", "hola")
-new Achievements ( 21, "Secreto de Pockles nº7", "EASTER EGG: Por poner 20 Pocks en un día", "hola")
+admin.database().ref('/achievements').push({name: '¡Bienvenido a Pockles!', description: 'Te has unido a la familia Pockles', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: '¡Primer Pock!', description: 'Has publicado tu primer pock. ¡Enhorabuena!', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: '¡10 Pocks!', description: 'Has publicado tu décimo pock. ¡Enhorabuena!', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: '¡100 Pocks!', description: 'Has publicado tu centésimo pock. ¡Enhorabuena!', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: '¡1000 Pocks!', description: 'Has publicado tu milésimo pock. ¡Enhorabuena!', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: '¡Tienes 10 Me Gusta!', description: 'Uno de tus Pocks ha gustado a 10 personas.', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: '¡Tienes 100 Me Gusta!', description: 'Uno de tus Pocks ha gustado a 100 personas ¡Wow!', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: '¡Has chateado con 5 personas!', description: 'Has iniciado una conversación con 5 usuarios.', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: '¡Has filtrado la lista de Pocks!', description: 'No todo quieren ver las mismas cosas...', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: '¡Has sido reportado!', description: 'Cuidado que sé donde vives, ¿eh? Vamos a portarnos bien.', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: '¡Has usado el mapa de calor!', description: 'Que calor hace... ¡Ah no! Que son los Pocks.', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: '¡Menuda puntería!', description: 'Has colocado dos Pocks exactamente en las mismas coordenadas. Rarito...', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: '¡Has mirado tu historial de Pocks!', description: 'Has mirado tu registro de Pocks. ¿De verdad pusiste eso?', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: '¡Llevas un año en Pockles!', description: 'Nadie se lo esperaba. Cuuumpleaños Feeeliz...', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: '¡Conseguiste todos los logros de Pockles!', description: '¡Eres un auténtico maestro de Pockles!', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: 'Secreto de Pockles nº1', description: 'EASTER EGG: Por crear 517 Pocks', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: 'Secreto de Pockles nº2', description: 'EASTER EGG: Por crear 13 Pocks de la categoría SALUD', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: 'Secreto de Pockles nº3', description: 'EASTER EGG: Por no poner ningún Pock durante un mes', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: 'Secreto de Pockles nº4', description: 'EASTER EGG: Por poner un Pock a las 8:00 AM', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: 'Secreto de Pockles nº5', description: 'EASTER EGG: Por mandarte 100 mensajes en un chat', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: 'Secreto de Pockles nº6', description: 'EASTER EGG: Por conseguir todos los logros normales', icon: 'lo que sea'})
+admin.database().ref('/achievements').push({name: 'Secreto de Pockles nº7', description: 'EASTER EGG: Por poner 20 Pocks en un día', icon: 'lo que sea'})
 
 
