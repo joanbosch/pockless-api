d20a51075f8f6b04cd405c8b1180aae72840d153
Vmorais22 <victormoraisllahi@gmail.com>
2020-05-06 15:25:26 +0200

    -debug

--- functions/src/modules/achievements/actions/achievement-controller.ts
+++ functions/src/modules/achievements/actions/achievement-controller.ts
@@ -13,22 +13,31 @@ import {Achievements} from "../models/achievements";
 
 export default async (user: any): Promise<Achievements[]> => {
 
-    //Step 1: Take an Snapshot of the Achievments
-    const snapshotOfAllAchivments = await admin.database().ref(OWNED_ACHIEVEMENTS_REF)
+    //Step 1: Take an Snapshot of the achievements of an user
+    const AllTheAchievmentsOfAnUser = await admin.database().ref(OWNED_ACHIEVEMENTS_REF)
+        .orderByChild('userId')
+        .equalTo(user.uid)
         .once('value')
 
-    //Step2: Get All Achievements where userId is equal to the user given
-    const allAchievmentsOfAnUser: Achievements[] = []
-    snapshotOfAllAchivments.forEach((s: admin.database.DataSnapshot) => {
-        if (s.val().userId == user) {
-            const snapshotOfTheAchivment = admin.database().ref(`${ACHIEVEMENTS_REF}/${s.val().achievementId}`).once('value')
-            snapshotOfAllAchivments.forEach((ss: admin.database.DataSnapshot) => {
-                allAchievmentsOfAnUser.push(ss.val())
+    //Step2: Get All AchievementsId of the user
+    const allAchivementsIdOfAnUser: String[] = []
+    AllTheAchievmentsOfAnUser.forEach((s: admin.database.DataSnapshot) => {
+                allAchivementsIdOfAnUser.push(s.val().achievementId)
             })
-        }
 
-    })
-    //Step 3: Return all Achievements
+    //Step3: Get all the Achievements
+    const allAchievmentsOfAnUser: Achievements[] =[]
+    for (let i in allAchivementsIdOfAnUser) {
+        const snapshotOfTheAchievements = await admin.database().ref(ACHIEVEMENTS_REF)
+            .orderByChild('achievementId')
+            .equalTo(i)
+            .once('value')
+        //we will only have 1 achievemnt in each snapshot
+        snapshotOfTheAchievements.forEach((ss: admin.database.DataSnapshot) => {
+            allAchievmentsOfAnUser.push(ss.val())
+        })
+    }
+    //Step 4: Return all Achievements
     return allAchievmentsOfAnUser
 }
 
