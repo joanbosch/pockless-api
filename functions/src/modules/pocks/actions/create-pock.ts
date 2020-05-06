39f409143e1438b97bda910e8c8ff6de1b323137
Vmorais22 <victormoraisllahi@gmail.com>
2020-05-06 17:54:51 +0200

    -Added achievement id references -Added most of the create-pock Achivement checks

--- functions/src/modules/pocks/actions/create-pock.ts
+++ functions/src/modules/pocks/actions/create-pock.ts
@@ -4,6 +4,18 @@ import { MESSAGES_REF } from "../../../common/paths";
 import addLocation from '../../geolocation/actions/add-location'
 import { CreatePockRestInput } from "../models/create-pock-rest-input"
 import { PockMessage } from "../models/pock-message"
+import {userGetNewAchievement} from "../../achievements/actions/achievement-checker";
+import {
+    EASTER_EGG_1,
+    EASTER_EGG_2, EASTER_EGG_4,
+    FIRST_POCK,
+    HUNDRED_POCK,
+    SAME_COORDS,
+    TEN_POCK,
+    THOUSAND_POCK,
+    WELCOME_POCKLES
+} from "../../achievements/achivements";
+import moment from "moment";
 
 export const DEFAULT_EXPIRATION_TIME = 7 * 24 * 3600 * 1000 // 1 week
 
@@ -49,25 +61,37 @@ export default async (input: CreatePockRestInput, user: any): Promise<PockMessag
     // Step 4: Return the freshly inserted pock from the database
     const pockInserted = await admin.database().ref(`${MESSAGES_REF}/${id}`).once('value')
 
-    //Check Achievement: Primer Pock
 
-    //Check Achievement: 10 Pock
+    //Achievement Check
+    const now = moment()
+    if(now.format("hh:mm") == "08:00") userGetNewAchievement(user.uid, EASTER_EGG_4)
 
-    //Check Achievement: 100 Pock
+    const snapshotAllPocks = await admin.database().ref(MESSAGES_REF)
+        .orderByChild("user").equalTo(user.uid)
+        .once('value')
+    const pocks: PockMessage[] = []
+    snapshotAllPocks.forEach((s: admin.database.DataSnapshot) => {
+        pocks.push(new PockMessage(s.val()))
+    })
 
-    //Check Achievement: 1000 Pock
+    if (pocks.length == 1) userGetNewAchievement(user.uid, FIRST_POCK)
+    if (pocks.length == 10) userGetNewAchievement(user.uid, TEN_POCK)
+    if (pocks.length == 100) userGetNewAchievement(user.uid, HUNDRED_POCK)
+    if (pocks.length == 1000) userGetNewAchievement(user.uid, THOUSAND_POCK)
+    if (pocks.length == 517) userGetNewAchievement(user.uid, EASTER_EGG_1)
 
-    //Check Achievement: 517 Pock
+    let health_counter = 0
+    pocks.forEach( p => {
+        if (p.location == input.location) userGetNewAchievement(user.uid, SAME_COORDS)
+        if (p.category == "Salud" && health_counter < 13) ++health_counter
+        if (health_counter == 13) userGetNewAchievement(user.uid, EASTER_EGG_2)
+    })
 
-    //Check Achievement: 12 Pock salud
 
     //Check Achievement: 1 mes sin Pock
 
-    //Check Achievement: 1 pock a las 8:00Am
-
     //Check Achievement: 1 pock/dia durante 10
 
-    //Check Achievement: Que punteria
 
     return new PockMessage(Object.assign({}, pockInserted.val(), {id: pockInserted.key}))
 }
