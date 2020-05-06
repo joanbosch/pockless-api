0e28a895afca5d5cdb9786e169050c433ccc6e6b
Vmorais22 <victormoraisllahi@gmail.com>
2020-05-06 15:57:38 +0200

    -generalisizing

--- functions/src/modules/achievements/actions/achievement-controller.ts
+++ functions/src/modules/achievements/actions/achievement-controller.ts
@@ -2,8 +2,17 @@
 import admin from "firebase-admin";
 import {OWNED_ACHIEVEMENTS_REF} from "../../../common/paths";
 import {now} from "moment";
+import {composeKey} from "../../pocks/actions/like-pock";
 
 export const userGetNewAchievement = async (usId: string, achId: string) => {
+    // Step 1: check if the user has or not the acievement
+    const checkAchievement = await admin.database().ref(OWNED_ACHIEVEMENTS_REF)
+        .orderByChild("composedKey")
+        .equalTo(composeKey(usId, achId))
+        .once('value')
+
+    //Step 2: if not, add it
+    if (checkAchievement.val() == null)
     admin.database().ref(OWNED_ACHIEVEMENTS_REF).push({
         achievementId: achId,
         userId: usId,
--- functions/src/modules/user/actions/create-user.ts
+++ functions/src/modules/user/actions/create-user.ts
@@ -5,6 +5,7 @@ import { CreateUserRestInput, } from "../model/create-user-rest-input";
 import { UserProfile } from "../model/user-profile";
 import getUserExists from "./get-user-exists";
 import {userGetNewAchievement} from "../../achievements/actions/achievement-controller";
+import {composeKey} from "../../pocks/actions/like-pock";
 
 /**
  * Creates the user in the database if it does not exist
@@ -21,7 +22,9 @@ export default async (body: CreateUserRestInput): Promise<UserProfile> => {
 
     const user = await admin.database().ref(`${PROFILE_REF}/${body.id}`).once('value')
 
+    //Achievement Check
     userGetNewAchievement(body.id, "M6aXU24YSeV7FinkeFC")
+    //End Achievement Check
 
     return new UserProfile(user.val())
 }
