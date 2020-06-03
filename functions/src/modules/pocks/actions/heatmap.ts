8cf8c000b02c48d7737fb64bcb8383295d0b7bc7
Victor <victorblancogarcia3@gmail.com>
2020-06-03 18:07:58 +0200

    fix heatmap

--- functions/src/modules/pocks/actions/heatmap.ts
+++ functions/src/modules/pocks/actions/heatmap.ts
@@ -1,20 +1,17 @@
 import * as admin from "firebase-admin"
-import {now} from "moment";
-import {LatLong} from "../../../common/models/lat-long"
-import {MESSAGES_REF} from "../../../common/paths";
+import { now } from "moment";
+import { LatLong } from "../../../common/models/lat-long"
+import { MESSAGES_REF } from "../../../common/paths";
 
 export default async (): Promise<LatLong[]> => {
 
     //Step 1: Take an Snapshot of the Pocks and treat errors
-    const snapshotOfAllPocks = await admin.database().ref(MESSAGES_REF)
-        .orderByChild('dateExpiration')
-        .endAt(now())
-        .once('value')
+    const snapshotOfAllPocks = await admin.database().ref(MESSAGES_REF).once('value')
 
     //Step2: Get All pock's location of the Snapshot
     const allPocksLocation: LatLong[] = []
     snapshotOfAllPocks.forEach((s: admin.database.DataSnapshot) => {
-        if (!s.val().hidden) {
+        if (!s.val().hidden && s.val().dateExpiration >= now()) {
             allPocksLocation.push(new LatLong(s.val().location))
         }
     })
